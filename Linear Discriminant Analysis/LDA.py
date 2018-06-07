a1cb5d4459e6d750c18b72d2eb754554de3f0efd
huangdaoxu <382619605@qq.com>
2018-06-07 15:26:39 +0800

    update lda kernel

--- Linear Discriminant Analysis/LDA.py
+++ Linear Discriminant Analysis/LDA.py
@@ -2,67 +2,63 @@ __author__ = 'huangdaoxu'
 
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-def class_mean(x, y):
-    classes = set(y)
-    mean = []
-    for cls in classes:
-        mean.append(np.mean(x[y == cls,:], axis=0))
-    return np.array(mean), classes
-
-# 计算类内散度矩阵
-def min_Sw(x, y):
-    mean, classes = class_mean(x, y)
-    Sw = np.zeros((x.shape[1], x.shape[1]))
-    for cls, m in zip(classes, mean):
-        Sw += np.dot((x[y == cls, :] - m).T, (x[y == cls, :] - m))
-    return Sw
-
-# 计算内间散度矩阵
-def max_Sb(x, y):
-    all_mean = np.mean(x, axis=0)
-    mean, classes = class_mean(x, y)
-    Sb = np.zeros((x.shape[1], x.shape[1]))
-    for cls, m in zip(classes, mean):
-        each_count = (y == cls).shape[0]
-        Sb += each_count * np.dot((m - all_mean).T, (m - all_mean))
-    return Sb
-
-# 计算Sw^-1 * Sb的特征向量以及特征值，排序筛选出最大的n_components个特征值以及特征向量，
-# 将特征向量组合成W，从而数据集点乘W得到降维后的数据
-def lda(x, y, n_components=2):
-    Sw = min_Sw(x, y)
-    Sb = max_Sb(x, y)
-    eig_vals, eig_vecs = np.linalg.eig(np.dot(np.linalg.inv(Sw), Sb))
-    eig_pairs = [[np.abs(eig_vals[i]), eig_vecs[:, i]] for i in range(len(eig_vals))]
-    eig_pairs = sorted(eig_pairs, key=lambda k: k[0], reverse=True)
-    class_count = len(set(y))
-    if n_components < class_count:
-        if n_components==2:
-            W = np.hstack((eig_pairs[0][1].reshape(x.shape[1], 1), eig_pairs[1][1].reshape(x.shape[1], 1)))
-        else:
-            W = ...
-    else:
-        raise ValueError('n_components must less than class count.')
-    return W
-
-
 from sklearn.datasets import load_iris
-data_set = load_iris()
-X,y = data_set.data,data_set.target
 
-w = lda(X, y, n_components=2)
-new_X = np.abs(X.dot(w))
-fig = plt.figure()
-plt.scatter(new_X[:, 0], new_X[:, 1],marker='o',c=y)
-plt.show()
+class LDA(object):
+    def __init__(self, n_components=2):
+        self.n_components=n_components
 
-# 对比sklearn中实现的基于特征值与特征向量的LDA线性分析方法，经过源码比较后发现多分类实现有点区别。
-# sklearn通过全局散度矩阵减去类内散度矩阵来求得类间散度矩阵，sklearn的实现更
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-lda = LinearDiscriminantAnalysis(solver='eigen', n_components=2)
-lda.fit(X,y)
-X_new = lda.transform(X)
-plt.scatter(X_new[:, 0], X_new[:, 1],marker='o',c=y)
-plt.show()
+    def _class_mean(self, x, y):
+        classes = set(y)
+        mean = []
+        for cls in classes:
+            mean.append(np.mean(x[y == cls, :], axis=0))
+        return np.array(mean), classes
+
+    # 计算类内散度矩阵
+    def _min_Sw(self, x, y):
+        mean, classes = self._class_mean(x, y)
+        Sw = np.zeros((x.shape[1], x.shape[1]))
+        for cls, m in zip(classes, mean):
+            Sw += np.cov((x[y == cls, :]).T, bias=True)
+        Sw /= len(classes)
+        return Sw
+
+    # 计算内间散度矩阵, 多分类使用St-Sw得到
+    def _max_Sb(self, x, y):
+        all_mean = np.mean(x, axis=0)
+        mean, classes = self._class_mean(x, y)
+        Sb = np.zeros((x.shape[1], x.shape[1]))
+        for cls, m in zip(classes, mean):
+            each_count = (y == cls).shape[0]
+            Sb += each_count * np.dot((m - all_mean).T, (m - all_mean))
+        return Sb
+
+    # 计算Sw^-1 * Sb的特征向量以及特征值，排序筛选出最大的n_components个特征值以及特征向量，
+    # 将特征向量组合成，从而数据集点乘特征向量得到数据
+    def _lda(self, x, y):
+        Sw = self._min_Sw(x, y)
+        St = np.cov(x.T, bias=True)
+        Sb = St - Sw
+        eig_vals, eig_vecs = np.linalg.eig(np.dot(np.linalg.inv(Sw), Sb))
+        eig_vecs = eig_vecs[:, np.argsort(eig_vals)[::-1]]
+        return eig_vecs
+
+    def transform(self, x, y):
+        class_count = len(set(y))
+        if self.n_components < class_count:
+            w = self._lda(x, y)
+            new_X = np.dot(X, w)[:, :2]
+        else:
+            raise ValueError('n_components must less than class count.')
+        return new_X
+
+
+if __name__ == '__main__':
+    data_set = load_iris()
+    X, y = data_set.data, data_set.target
+
+    lda = LDA(n_components=2)
+    new_X = lda.transform(X, y)
+    plt.scatter(new_X[:, 0], new_X[:, 1],marker='o',c=y)
+    plt.show()
--- README.md
+++ README.md
@@ -10,4 +10,4 @@ recommendSys 是一个以numpy为基础编写的推荐系统，分别为基于�
 
 autoencoder为自编码模型，它属于非监督学习，该模型常用于提取有效特征以及降低特征维度（作用与PCA降维类似），需要注意的一点是，对于激活函数的选取非常重要，在不同的特征集上需要找到最合适的激活函数。
 
-Linear Discriminant Analysis使用（python3.6 | numpy1.14）环境编写的线性判别分析模型，核心为通过特征值与特征向量来做变换，而sklearn中有三种kernel。它属于监督学习方式的特征降维，而PCA是非监督学习。
+Linear Discriminant Analysis使用（python3.6 | numpy1.14）环境编写的线性判别分析模型，核心为通过特征值分解，而sklearn中有三种kernel。它属于监督学习方式的特征降维，而PCA是非监督学习。理论知识及公式基于周志华老师的机器学习书籍。
