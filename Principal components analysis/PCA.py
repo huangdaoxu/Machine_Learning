ae8c04c61b36a06fc03cf4982f93dfc268450a5a
huangdaoxu <382619605@qq.com>
2018-06-07 18:04:13 +0800

    create PCA file

--- Principal components analysis/PCA.py
+++ Principal components analysis/PCA.py
@@ -3,7 +3,6 @@ __author__ = 'huangdaoxu'
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
-from sklearn.decomposition import PCA
 
 class PCA(object):
     """
